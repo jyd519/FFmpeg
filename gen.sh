f64918f35c4baddb5d06a608cd2150fc4ffd1fc8
jiyongdong <jiyongdong@ata.net.cn>
2024-08-07 09:14:10 +0800

    build: add rtmp protocol

--- gen.sh
+++ gen.sh
@@ -2,16 +2,15 @@
             --prefix=/ffbuild --disable-autodetect \
             --enable-version3 --pkg-config-flags="--static" --pkg-config=pkg-config \
             --disable-ffplay --disable-iconv \
-            --enable-shared --disable-static --disable-protocols --enable-protocol=file \
+            --enable-shared --disable-static \
+            --disable-protocols --enable-protocol=file --enable-protocol=rtmp \
             --disable-libxcb --disable-xlib \
             --enable-zlib --enable-libaom \
             --enable-libmp3lame --enable-libopenh264 --enable-libvpx --enable-libvorbis \
             --enable-libopus --enable-libfdk-aac \
             --disable-libx264 --disable-libx265 --disable-libplacebo --disable-libxvid \
             --enable-decoder=libopus --enable-encoder=libopus \
-            --disable-network \
             --extra-cxxflags= --extra-ldexeflags= --extra-libs=-lgomp \
+            --enable-network \
             --extra-ldflags="-static-libgcc --static" --extra-cflags="-DLIBTWOLAME_STATIC" \
             --extra-version=20240214
-            
-#--disable-indevs --disable-outdevs \
